3435d1915284a783bde7f6b7b55d0e8cd77b3bfe
wlk <william.koelling@gmail.com>
2022-11-08 18:15:55 -0600

    setup models

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    }
+)
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    }
+)
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username : {
+            type : String,
+            unique : true,
+            required : true,
+            trim : true
+        },
+        email : {
+            type : String,
+            required : true,
+            unique : true,
+            match: [/.+\@.+\..+/, "Invalid email supplied."],
+
+        },
+        thoughts : [
+            {
+                type : Schema.Types.ObjectId,
+                ref: "thought"
+            }
+        ]
+    }
+)
+
+const User = model("user", userSchema);
+
+module.exports = User;
